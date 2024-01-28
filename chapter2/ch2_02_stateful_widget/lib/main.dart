2c70956aaaf8ee9c8210e49f332413f6e1d6d4b5
nico25-orange <masa25hw@gmail.com>
2024-01-29 02:24:43 +0900

    feat: switch the display of widget according to counter

--- chapter2/ch2_02_stateful_widget/lib/main.dart
+++ chapter2/ch2_02_stateful_widget/lib/main.dart
@@ -56,7 +56,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  String _type = "Even";
 
   void _incrementCounter() {
     setState(() {
@@ -66,11 +65,6 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
-      if (_counter % 2 == 0) {
-        _type = "Even";
-      } else {
-        _type = "Odd";
-      }
     });
   }
 
@@ -118,10 +112,9 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-            Text(
-              _type,
-              style: const TextStyle(fontSize: 20, color: Colors.red),
-            ),
+            if (_counter % 2 == 0)
+              const Text('Even',
+                  style: TextStyle(fontSize: 20, color: Colors.red)),
           ],
         ),
       ),
